1265631e581e7934e91f011d50f6ec34ed7b2a6e
Shivam Saraswat <44198501+shivamsaraswat@users.noreply.github.com>
2020-11-14 14:36:47 +0530

    Update netmappy.py

--- netmappy.py
+++ netmappy.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 from colorama import Fore, Style            
 print(Fore.CYAN + r"@@    @ @$$$$$$$ $$$$$$$ @@    @@     @@     @$$$$$$ @$$$$$$ @     @")
 print(Fore.CYAN + r"@ @   @ @           @    @ @  @ @    @  @    @     $ @     $  @   @ ")
